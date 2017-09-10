49d95c2d02075fb29e4fb54fff394371e73833bb
luoluoluo <360646101@qq.com>
2017-09-10 10:56:25 +0800

    * protobuf composer

---
+++ gen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+root=$(cd "$(dirname "$0")"; pwd)
+srcpath=$root/../protobuf
+outpath=$root/src/
+if [[ ! -d "$outpath" ]];then
+    mkdir -p $outpath
+fi
+rm -rf $outpath/*
+protoc -I $srcpath --php_out=$outpath --grpc_out=$outpath --plugin=protoc-gen-grpc=/opt/protobuf/bin/grpc_php_plugin $srcpath/*.proto
---
+++ src/Protobuf/AccountClient.php
@@ -0,0 +1,89 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace Protobuf;
+
+/**
+ */
+class AccountClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * 登陆
+     * @param \Protobuf\LoginRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function Login(\Protobuf\LoginRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/protobuf.Account/Login',
+        $argument,
+        ['\Protobuf\LoginResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * 注册
+     * @param \Protobuf\RegisterRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function Register(\Protobuf\RegisterRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/protobuf.Account/Register',
+        $argument,
+        ['\Protobuf\RegisterResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * 登陆校验
+     * @param \Protobuf\AuthRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function Auth(\Protobuf\AuthRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/protobuf.Account/Auth',
+        $argument,
+        ['\Protobuf\AuthResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * 修改密码
+     * @param \Protobuf\ModifyPasswordRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function ModifyPassword(\Protobuf\ModifyPasswordRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/protobuf.Account/ModifyPassword',
+        $argument,
+        ['\Protobuf\ModifyPasswordResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * 重置密码
+     * @param \Protobuf\ResetPasswordRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function ResetPassword(\Protobuf\ResetPasswordRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/protobuf.Account/ResetPassword',
+        $argument,
+        ['\Protobuf\ResetPasswordResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
